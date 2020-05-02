0fb84f2a70505e4f5d7a578b664ff15428e32d8f
Maiduydung <maiduydung@yahoo.com>
2020-05-02 15:12:26 +0900

    added note on list

--- 2nd week/NOTES.ml
+++ 2nd week/NOTES.ml
@@ -63,4 +63,66 @@ let scarlar =
   fun n (x, y) -> (x *. n, y *. n);;
 let (x, y) = scarlar 3.0 (1.0, 2.1);;
 
-let get_
+let get_second(_, s) = s in 
+  get_second ("Duy", "Dung");;
+
+(* Patterns need to stay exhaustive *)
+let flip = function
+  0 -> 1 | 1 -> 0;;
+    (* 
+    Warning 8: this pattern-matching is not exhaustive.
+    Here is an example of a case that is not matched: 2 
+    *)
+    (* # flip 2;;
+    Exception: Match_failure ("//toplevel//", 1, 11). *)
+    
+
+(* List *)
+  
+  (* Empty list *)
+  [];;
+
+  (* :: operator *)
+    (* Create a new list on top of list tail *)
+    (* head :: tail *)
+
+  (* Alternatives *)
+  1::(2::(3::[]));;
+    (* - : int list = [1; 2; 3] *)
+
+  0::[1;2;3];;
+    (* - : int list = [0; 1; 2; 3] *)
+  
+  (* Use operator :: to split list into the first element and the rest *)
+  let rec list_sum lst =
+    match lst with
+      head::tail -> head + (list_sum tail)
+      | [] -> 0;;
+  
+  list_sum [1;2;3;4;5];;
+    (* 
+    # list_sum [1;2;3;4;5];;
+    - : int = 15 
+    *)
+
+  (* @ operator to concatenate 2 list *)
+  [1;2] @ [3;4;5;6];;
+    (* - : int list = [1; 2; 3; 4; 5; 6] *)
+    (* list_1 @ list_2 = Big O of (list_1.size) *)
+    (* for example *)
+    let rec list_con l1 l2 =
+      match l1 with
+      [] ->l2
+      | hd::tl -> hd::(tl @ l2);;
+
+    list_con [1;2;3] [4;5;6];;
+      (* - : int list = [1; 2; 3; 4; 5; 6] *)
+
+  (* Pattern matching with list *)
+  let sum_3 lst = 
+    match lst with
+    [a;b;c] -> a + b + c
+    | _ -> failwith "error";;
+    
+  sum_3 [7;3;2];;
+    (* - : int = 12 *)
