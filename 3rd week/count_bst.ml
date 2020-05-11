93c377073ba89c2020ee53bf1d8605fa78a2866a
Maiduydung <maiduydung@yahoo.com>
2020-05-11 23:30:34 +0900

    count nodes in BST

--- 3rd week/count_bst.ml
+++ 3rd week/count_bst.ml
@@ -1,5 +1,17 @@
+
+type 'a btree = 
+  Node of 'a * 'a btree * 'a btree
+  | Leaf;;
+
+
+let btree = Node(4, Node(2, Leaf, Node(10, Leaf, Leaf)), Node(3, Leaf, Leaf));;
+
+
+
 let rec count_bst tr = 
   match tr with 
-    Node(_, tr1, tr2) -> 1 + 
-    | Leaf -> 
+    Node(_, tr1, tr2) -> 1 + (count_bst tr1) + (count_bst tr2)
+    | Leaf -> 0
 ;;
+
+count_bst btree;;
