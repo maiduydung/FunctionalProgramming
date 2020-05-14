afacad85e14a838fb187fc0274ec3ffb13d7f77d
Maiduydung <maiduydung@yahoo.com>
2020-05-14 17:46:55 +0900

    added exception handling

--- 4th week/repl.ml
+++ 4th week/repl.ml
@@ -4,6 +4,7 @@ type expression =
   Binop of char * expression * expression
 | Number of int
 
+exception ParseError;;
 let parse expr =
 let expr = 
   let r = ref "" in
@@ -27,11 +28,11 @@ and parse_muldiv pos =
       | _ -> (pos, e)
   in iter (parse_simple pos)
 and parse_simple pos =
-  if pos >= len then failwith "Parse error"
+  if pos >= len then raise ParseError
   else match expr.[pos] with
       '(' ->
       let pos, head = parse_addsub (pos+1) in
-      if pos >= len || expr.[pos] <> ')' then failwith "Parse error"
+      if pos >= len || expr.[pos] <> ')' then raise ParseError
       else (pos+1), head
     | c when c >= '0' && c <= '9' ->
       let char_of_int c = Char.code c - Char.code '0' in
@@ -40,13 +41,13 @@ and parse_simple pos =
         else iter (pos+1, v * 10 + char_of_int expr.[pos])
       in let (pos, v) = iter (pos+1, char_of_int c)
       in pos, Number(v)
-    | _ -> failwith "Parse error"
+    | _ -> raise ParseError
 in let pos, head = parse_addsub 0 in
-if pos < len then failwith "Parse error"
+if pos < len then raise ParseError
 else head
 ;;
 
-
+exception EvalError;;
 let rec eval exp = 
   match exp with
   | Binop('+',tr1,tr2) -> (eval tr1) + (eval tr2)
@@ -54,16 +55,23 @@ let rec eval exp =
   | Binop('*',tr1,tr2) -> (eval tr1) * (eval tr2)
   | Binop('/',tr1,tr2) -> (eval tr1) / (eval tr2)
   | Number(num) -> num
-  | Binop(_,_,_) -> failwith "error";;
+  | Binop(_,_,_) -> raise EvalError;;
 
 
 let rec repl () = 
-  print_string ">>> ";
-  let command = read_line() in
-  let expr = parse command in 
-  let res = eval expr in 
-  print_endline (string_of_int res);
-  repl()
+  begin
+    try
+      print_string ">>> ";
+      let command = read_line() in
+      let expr = parse command in 
+      let res = eval expr in 
+      print_endline (string_of_int res);
+    with
+        ParseError -> print_endline "ERROR: syntax error" 
+      | EvalError -> print_endline "ERROR: evaluation error" 
+      | Division_by_zero -> print_endline "ERROR: devision by zero"
+  end;
+  repl() 
 ;;
 
 repl() ;;
