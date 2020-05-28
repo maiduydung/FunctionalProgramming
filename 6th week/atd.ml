8cf089c871df8c45b0e4d7bcb12b436d12d26a8e
Maiduydung <maiduydung@yahoo.com>
2020-05-28 16:51:42 +0900

    module name must start with a capital letter

---
+++ 6th week/atd.ml
@@ -0,0 +1,13 @@
+  (* module name must start with a capital letter *)
+  
+  module Iniad :
+    sig
+
+      val is : unit -> string 
+    end =
+
+    struct 
+
+      let is () = "Information Networking for Innovation and Design\n"
+
+    end;;
