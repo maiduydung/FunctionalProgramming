78e8b345eba2c577e4843e97ad88e7cd4a66d77b
Maiduydung <maiduydung@yahoo.com>
2020-07-02 15:54:20 +0900

    init

---
+++ 12th week/NOTES.ml
@@ -0,0 +1,7 @@
+Register Allocation
+    Say we have an infinite amount of register, then ignoring the performance, put every variable to a register
+    
+    Obiviously, registers are finite.
+        Reusing registers: assigning new value replacing no longer needed value
+        Coalescing variable: allocate same register for multiple variable with the same value
+        
