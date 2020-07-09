727482b55c0ea14c464ab384e70d8fd710693693
Maiduydung <maiduydung@yahoo.com>
2020-07-09 17:09:41 +0900

    init

---
+++ 13th week/K_form.c
@@ -0,0 +1,13 @@
+int add_five_num(int a, int b, int c, int d, int e){
+   return a + b + c + d + e;
+}
+
+int add_five_num(int __t1, int __t2, int __t3, int __t4, int __t5)
+{
+   int __t0;
+   __t0 = __t1 + __t2;
+   __t0 = __t0 + __t3;
+   __t0 = __t0 + __t4;
+   __t0 = __t0 + __t5;
+   return __t0
+}
